543a1d4bdae610f83edfbd2879cb6415c3a91429
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-08-01 00:22:53 -0300

    Auto sync connected wallet with authenticated user account

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,25 +1,20 @@
 import RaffleCard from "@/components/RaffleCard";
 import Leaderboard from "@/components/Leaderboard";
-import React from "react";
-import { UserCard } from "@/components/UserCard";
-import { WagerHistory } from "@/components/WagerHistory";
+import WalletWagerSide from '@/components/wallet-history-side/WalletWagerSide';
 
 export default function Home() {
-    return (
-        <div className="flex h-[calc(100vh-2rem)]">
-            <div className="relative w-full  rounded-xl p-px shadow-2xl overflow-hidden">
-                <div className="card-border-animated"></div>
-                <div className="relative z-10 h-full w-full rounded-[15px] bg-card p-8 overflow-auto">
-                    <RaffleCard />
-                    <div className="mt-5">
-                        <Leaderboard />
-                    </div>
-                </div>
-            </div>
-            <div className="relative w-[20%] rounded-xl ps-4 overflow-hidden">
-                <UserCard />
-                <WagerHistory />
-            </div>
+  return (
+    <div className="flex h-[calc(100vh-2rem)]">
+      <div className="relative w-full  rounded-xl p-px shadow-2xl overflow-hidden">
+        <div className="card-border-animated"></div>
+        <div className="relative z-10 h-full w-full rounded-[15px] bg-card p-8 overflow-auto">
+          <RaffleCard />
+          <div className="mt-5">
+            <Leaderboard />
+          </div>
         </div>
-    );
+      </div>
+      <WalletWagerSide />
+    </div>
+  );
 }
