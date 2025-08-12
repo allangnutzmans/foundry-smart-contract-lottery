df225da6f1663fc5940039540f04498efce1040f
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-08-11 22:51:27 -0300

    Use correct tags for aside content

--- web/src/components/wallet-history-side/WalletWagerSide.tsx
+++ web/src/components/wallet-history-side/WalletWagerSide.tsx
@@ -6,9 +6,9 @@ import { useWalletSync } from '@/hooks/useWalletSync';
 export default function WalletWagerSide() {
   useWalletSync(); // Ensure wallet is synced with user account
   return (
-    <div className="relative w-[20%] rounded-xl ps-4 overflow-hidden">
+    <aside className="relative w-[20%] rounded-xl ps-4 overflow-hidden">
       <UserCard />
       <WagerHistory />
-    </div>
+    </aside>
   );
 }
