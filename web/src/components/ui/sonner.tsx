ce81878f1d3ad5579e6e9d921ec29b9179b7067f
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-08-24 21:42:20 -0300

    Simple ui color fix

--- web/src/components/ui/sonner.tsx
+++ web/src/components/ui/sonner.tsx
@@ -14,7 +14,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
         {
           "--normal-bg": "var(--popover)",
           "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
+          "--normal-border": "var(--ring)",
         } as React.CSSProperties
       }
       {...props}
