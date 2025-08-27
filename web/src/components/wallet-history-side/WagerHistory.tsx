39705d6dcefc0a68400449f26b350e943bad7450
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-08-26 22:21:04 -0300

    simple type removal

--- web/src/components/wallet-history-side/WagerHistory.tsx
+++ web/src/components/wallet-history-side/WagerHistory.tsx
@@ -64,7 +64,7 @@ export const WagerHistory = () => {
         ) : (
 
             <div className="grid gap-3">
-              {wagerHistory?.map((wager: WagerHistoryItem) => (
+              {wagerHistory?.map((wager) => (
                 <WagerHistoryCard key={wager.id} wager={wager} />
               ))}
             </div>
